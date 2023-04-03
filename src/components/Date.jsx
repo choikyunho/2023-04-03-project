8dccbd6ca7f01cee174c7cf0ced3591c05977b34
choikyunho <rbsgh777@naver.com>
2023-04-03 11:54:47 +0900

    map fuction

---
+++ src/components/Date.jsx
@@ -0,0 +1,15 @@
+function Date(){
+    return (
+        <li className ="lnline-block bg-white rounded-lg shadow-md">
+            <div className ="bg-red-400 rounded-t-lg px-2 text-xl text-white">
+                2
+            </div>
+            <ul className ="p-2 text-lg">
+            <li>🧹 청소하기</li>
+            <li>👕 빨래하기</li>
+            </ul>
+        </li>
+    )
+}
+
+export default Date;
