2d27bf15f8e8fae3b380e7564dcc5c83eb193a56
choikyunho <rbsgh777@naver.com>
2023-04-03 09:45:29 +0900

    components

---
+++ src/components/CardB.jsx
@@ -0,0 +1,5 @@
+function CardB() {
+    return <div className="bg-blue-100 w-40 h-60">B</div>;
+  }
+  
+  export default CardB;
---
+++ src/components/CardF.jsx
@@ -0,0 +1,5 @@
+function CardF() {
+    return <div className="bg-orange-100 w-40 h-60">F</div>;
+  }
+  
+  export default CardF;
