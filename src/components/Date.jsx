0d2191f2abdf575ae55f2b9ac9173d73d089b271
choikyunho <rbsgh777@naver.com>
2023-04-03 14:30:25 +0900

    ternary operator use

--- src/components/Date.jsx
+++ src/components/Date.jsx
@@ -1,10 +1,13 @@
 function Date(props) {
     return (
       <li className="inline-block bg-white rounded-lg shadow-md">
-        <div className={`${props.day ==="SUN"?"bg-red-400":"bg-blue-400"} rounded-t-lg px-2 py-1 text-xl text-white`}>
+        <div 
+       className={`${
+        props.day === "SUN" ? "bg-red-400" : props.day === "SAT" ? "bg-orange-400" : "bg-blue-400"} 
+        rounded-t-lg px-2 py-1 text-xl text-white`}>
           {props.date}
         </div>
-        <ul className="p-2 text-lg">
+        <ul className="p-2 text-lg h-20">
           {props.todos.map((v, i) => {
             return <li>{v}</li>;
           })}
