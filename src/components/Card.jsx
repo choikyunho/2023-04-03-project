4998b2216902c9babcbaf5a8a507efd0c12a39f1
choikyunho <rbsgh777@naver.com>
2023-04-03 10:11:41 +0900

    Destructuring assignment

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,5 @@
-function Card(props) {
-    return <div className={`${props.color} w-40 h-60`}>{props.cardName}</div>;
+function Card({color,cardName}) {
+    return <div className={`${color} w-40 h-60`}>{cardName}</div>;
   }
   
   export default Card;
