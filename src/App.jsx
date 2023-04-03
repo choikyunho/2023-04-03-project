2a8d3ad5f5acbc105bca239c90fd2194341c3199
choikyunho <rbsgh777@naver.com>
2023-04-03 09:36:25 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import CardA from "./components/CardA";
+
+function App() {
+  return (
+    <div className="min-h-screen p-12 grid grid-cols-3">
+      <CardA />
+    </div>
+  );
+}
+
+export default App;
